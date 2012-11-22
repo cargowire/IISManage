75fe1c7a8408004727ddede047203dc0cef95610
tombuildsstuff <git@ibuildstuff.co.uk>
2012-11-22 11:21:56 +0000

    Made changes so that now 'master.test.me.com' is created, and also aliased as 'test.me.com' if 'master' is the default branch name

--- Program.cs
+++ Program.cs
@@ -1,83 +1,104 @@
-﻿using System;
-using System.IO;
-using Microsoft.Web.Administration;
-
-using NDesk.Options;
-
-namespace IISManage
+﻿namespace IISManage
 {
-	public class Program
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    using Microsoft.Web.Administration;
+
+    using NDesk.Options;
+
+    public class Program
 	{
 		public static void Main(string[] args)
 		{
-			var showHelp = false;
+			if (args != null && args.Any())
+                RunFromArgs(args);
+            else
+                CreateIISSite(@"C:\Tools\IISDemo\Sites\{0}\", "master.test.me.com", @"C:\Tools\IISDemo\Logs\{0}\", "Templates", "v4.0", "test.me.com", "master");
+		}
 
-			var sitename = string.Empty;
-			string branchsitename = null;
-			var sitesfolder = string.Empty;
-			var logsfolder = string.Empty;
-			var apppoolname = System.Configuration.ConfigurationManager.AppSettings["DefaultAppPool"];
-			var stringtoreplace = System.Configuration.ConfigurationManager.AppSettings["RemoveFromBranchName"];
-			var appPoolDotNetVersion = System.Configuration.ConfigurationManager.AppSettings["AppPoolDotNetVersion"];
+        public static void RunFromArgs(string[] args)
+        {
+            var showHelp = false;
 
-			var branch = string.Empty;
-			var defaultbranch = string.Empty;
+            var baseSiteUrl = string.Empty;
+            string branchSiteUrl = null;
+            var sitesFolder = string.Empty;
+            var logsFolder = string.Empty;
+            var appPoolName = System.Configuration.ConfigurationManager.AppSettings["DefaultAppPool"];
+            var stringToReplace = System.Configuration.ConfigurationManager.AppSettings["RemoveFromBranchName"];
+            var appPoolDotNetVersion = System.Configuration.ConfigurationManager.AppSettings["AppPoolDotNetVersion"];
 
-			var p = new OptionSet
+            var branch = string.Empty;
+            var defaultBranch = string.Empty;
+
+            var p = new OptionSet
 				{
-					{ "s|site=", "The name of the site to create (doubles as host header).", (v) => sitename = v },
-					{ "sf|sitefolder=", "The physical folder to store the site (is string formatted with the sitename).", (v) => sitesfolder = v },
-					{ "lf|logsfolder=", "The physical folder to store the logs of the site (is string formatted with the sitename).", (v) => logsfolder = v },
-					{ "a|apppool=", "The name of the application pool to use/create.", (v) => apppoolname = v },
+					{ "s|site=", "The name of the site to create (doubles as host header).", (v) => baseSiteUrl = v },
+					{ "sf|sitefolder=", "The physical folder to store the site (is string formatted with the sitename).", (v) => sitesFolder = v },
+					{ "lf|logsfolder=", "The physical folder to store the logs of the site (is string formatted with the sitename).", (v) => logsFolder = v },
+					{ "a|apppool=", "The name of the application pool to use/create.", (v) => appPoolName = v },
 					{ "b|branch=", "The branch (assuming source control) that is being used for this site.", (v) => branch = v },
-					{ "db|defaultbranch=", "The default branch (assuming source control) that is being used for this site.", (v) => defaultbranch = v },
+					{ "db|defaultbranch=", "The default branch (assuming source control) that is being used for this site.", (v) => defaultBranch = v },
 					{ "apv|apppoolversion=", "The version of .NET which the application pool runs in", (v) => appPoolDotNetVersion = v },
 					{ "h|help",  "show this message and exit", v => showHelp = v != null }
 				};
 
-			try
-			{
-				p.Parse(args);
+            try
+            {
+                p.Parse(args);
 
-				if (!string.IsNullOrEmpty(branch))
-				{
-					branch = branch.Replace(stringtoreplace, string.Empty);
+                if (!string.IsNullOrEmpty(branch))
+                {
+                    branch = branch.Replace(stringToReplace, string.Empty);
+                    branchSiteUrl = string.Concat(branch.ToLower(), ".", baseSiteUrl);
+                }
+                else
+                    branchSiteUrl = string.Concat(defaultBranch.ToLower(), ".", baseSiteUrl);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("IISManage: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try 'IISManage --help' for more information.");
+                return;
+            }
 
-					if (!branch.Equals(defaultbranch, StringComparison.InvariantCultureIgnoreCase))
-						branchsitename = string.Concat(branch, ".", sitename);
-				}
-			}
-			catch (OptionException e)
-			{
-				Console.Write("IISManage: ");
-				Console.WriteLine(e.Message);
-				Console.WriteLine("Try 'IISManage --help' for more information.");
-				return;
-			}
+            if (showHelp)
+            {
+                ShowHelp(p);
+                return;
+            }
 
-			if (showHelp)
-			{
-				ShowHelp(p);
-				return;
-			}
+            if (string.IsNullOrEmpty(baseSiteUrl))
+                throw new ArgumentNullException("site");
 
-			if (string.IsNullOrEmpty(sitename))
-				throw new ArgumentNullException("site");
+            if (string.IsNullOrEmpty(sitesFolder))
+                throw new ArgumentNullException("sitefolder");
 
-			if (string.IsNullOrEmpty(sitesfolder))
-				throw new ArgumentNullException("sitefolder");
+            CreateIISSite(sitesFolder, branchSiteUrl, logsFolder, appPoolName, appPoolDotNetVersion, baseSiteUrl, defaultBranch);
+        }
 
-			var sitelocation = CreateFolder(string.Format(sitesfolder, branchsitename ?? sitename));
-			var logs = CreateFolder(string.Format(logsfolder, branchsitename ?? sitename));
+        public static void CreateIISSite(string sitesFolder, string branchSiteName, string logsFolder, string appPoolName, string appPoolDotNetVersion, string baseSiteUrl, string defaultBranchName)
+        {
+            var siteDirectory = string.Format(sitesFolder, branchSiteName);
+            var logDirectory = string.Format(logsFolder, branchSiteName);
 
-			using (var serverManager = new ServerManager())
-			{
-				CreateApplicationPool(serverManager, apppoolname, appPoolDotNetVersion);
-				var site = CreateWebsite(serverManager, sitename, sitelocation, logs, apppoolname);
+            Console.WriteLine("Site Directory: {0}", siteDirectory);
+            Console.WriteLine("Log Directory: {0}", logDirectory);
 
-				serverManager.CommitChanges();
-			}
-		}
+            CreateFolder(siteDirectory);
+            CreateFolder(logDirectory);
+
+            using (var serverManager = new ServerManager())
+            {
+                CreateApplicationPool(serverManager, appPoolName, appPoolDotNetVersion);
+                var site = CreateWebsite(serverManager, branchSiteName, defaultBranchName, baseSiteUrl, siteDirectory, logDirectory, appPoolName);
+
+                serverManager.CommitChanges();
+            }
+        }
 
 		private static void ShowHelp(OptionSet p)
 		{
@@ -87,12 +108,10 @@ namespace IISManage
 			p.WriteOptionDescriptions(Console.Out);
 		}
 
-		private static DirectoryInfo CreateFolder(string path)
+		private static void CreateFolder(string path)
 		{
 			if (!Directory.Exists(path))
-				return Directory.CreateDirectory(path);
-			
-			return new DirectoryInfo(path);
+			    Directory.CreateDirectory(path);
 		}
 
 		private static ApplicationPool CreateApplicationPool(ServerManager serverManager, string appPool, string runtimeVersion)
@@ -106,15 +125,19 @@ namespace IISManage
 			return iisAppPool;
 		}
 
-		private static Site CreateWebsite(ServerManager serverManager, string site, DirectoryInfo sitelocation, DirectoryInfo logs, string appPool)
+		private static Site CreateWebsite(ServerManager serverManager, string branchSiteName, string defaultBranchName, string baseSiteUrl, string siteLocation, string logDirectory, string appPool)
 		{
-			var iisSite = serverManager.Sites[site];
+            var iisSite = serverManager.Sites[branchSiteName];
 			if (iisSite == null)
-				iisSite = serverManager.Sites.Add(site, "http", string.Format("*:80:{0}", site), sitelocation.ToString());
+			{
+			    iisSite = serverManager.Sites.Add(branchSiteName, "http", string.Format("*:80:{0}", branchSiteName), siteLocation);
+                if (!string.IsNullOrWhiteSpace(defaultBranchName) && branchSiteName.StartsWith(defaultBranchName, StringComparison.InvariantCultureIgnoreCase))
+                    iisSite.Bindings.Add(string.Format("*:80:{0}", baseSiteUrl), "http");
+			}
 
 			iisSite.ApplicationDefaults.ApplicationPoolName = appPool;
 
-			iisSite.LogFile.Directory = logs.ToString();
+            iisSite.LogFile.Directory = logDirectory;
 			iisSite.ServerAutoStart = true;
 
 			return iisSite;
